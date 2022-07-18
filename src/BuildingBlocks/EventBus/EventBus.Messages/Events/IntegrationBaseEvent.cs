f3649279031bde3697e401e62d68f8761b2878b7
Kiet Pham <kietpham.dev@gmail.com>
2022-07-18 13:51:20 +1000

    Create IntegrationBaseEvent

---
+++ src/BuildingBlocks/EventBus/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+
+namespace EventBus.Messages.Events;
+
+public record IntegrationBaseEvent
+{
+    public IntegrationBaseEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    [JsonConstructor]
+    public IntegrationBaseEvent(Guid id, DateTime createDate)
+    {
+        Id = id;
+        CreationDate = createDate;
+    }
+    
+    [JsonInclude]
+    public DateTime CreationDate { get; init; }
+
+    [JsonInclude]
+    public Guid Id { get; init; }
+}
