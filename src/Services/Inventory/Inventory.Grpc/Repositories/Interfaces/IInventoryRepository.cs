d14267c56bf208ecd4e5d4c33f72a7441de021bb
Kiet Pham <kietpham.dev@gmail.com>
2022-08-04 10:34:09 +1000

    update Inventory.Grpc project

---
+++ src/Services/Inventory/Inventory.Grpc/Repositories/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,9 @@
+using Contracts.Domains.Interfaces;
+using Inventory.Grpc.Entities;
+
+namespace Inventory.Grpc.Repositories.Interfaces;
+
+public interface IInventoryRepository : IMongoDbRepositoryBase<InventoryEntry>
+{
+    Task<int> GetStockQuantity(string itemNo);
+}
---
+++ src/Services/Inventory/Inventory.Grpc/Repositories/InventoryRepository.cs
@@ -0,0 +1,19 @@
+using Infrastructure.Common;
+using Inventory.Grpc.Entities;
+using Inventory.Grpc.Repositories.Interfaces;
+using MongoDB.Driver;
+using Shared.Configurations;
+
+namespace Inventory.Grpc.Repositories;
+
+public class InventoryRepository : MongoDbRepository<InventoryEntry>, IInventoryRepository
+{
+    public InventoryRepository(IMongoClient client, MongoDbSettings settings) : base(client, settings)
+    {
+    }
+
+    public async Task<int> GetStockQuantity(string itemNo)
+        => Collection.AsQueryable()
+            .Where(x => x.ItemNo.Equals(itemNo))
+            .Sum(x => x.Quantity);
+}
