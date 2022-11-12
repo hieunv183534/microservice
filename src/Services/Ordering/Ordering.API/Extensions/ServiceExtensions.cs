3fb2c0c84240d06fea85b68ab64bba48ed9b9064
Kiet Pham <kietpham.dev@gmail.com>
2022-11-12 20:48:47 +1100

    Completed 13.14

--- src/Services/Ordering/Ordering.API/Extensions/ServiceExtensions.cs
+++ src/Services/Ordering/Ordering.API/Extensions/ServiceExtensions.cs
@@ -1,9 +1,6 @@
 using Infrastructure.Configurations;
 using Infrastructure.Extensions;
-using MassTransit;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Ordering.API.Application.IntegrationEvents.EventsHandler;
 using Shared.Configurations;
 
 namespace Ordering.API.Extensions;
@@ -21,37 +18,9 @@ public static class ServiceExtensions
             .Get<DatabaseSettings>();
         services.AddSingleton(databaseSettings);
 
-        var eventBusSettings = configuration.GetSection(nameof(EventBusSettings))
-            .Get<EventBusSettings>();
-        services.AddSingleton(eventBusSettings);
-
         return services;
     }
 
-    public static void ConfigureMassTransit(this IServiceCollection services)
-    {
-        var settings = services.GetOptions<EventBusSettings>("EventBusSettings");
-        if (settings == null || string.IsNullOrEmpty(settings.HostAddress))
-            throw new ArgumentNullException("EventBusSetting is not configured");
-        
-        var mqConnection = new Uri(settings.HostAddress);
-        services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
-        services.AddMassTransit(config =>
-        {
-            config.AddConsumersFromNamespaceContaining<BasketCheckoutEventHandler>();
-            config.UsingRabbitMq((ctx, cfg) =>
-            {
-                cfg.Host(mqConnection);
-                // cfg.ReceiveEndpoint("basket-checkout-queue", c =>
-                // {
-                //     c.ConfigureConsumer<BasketCheckoutEventHandler>(ctx);
-                // });
-                
-                cfg.ConfigureEndpoints(ctx);
-            });
-        });
-    }
-    
     public static void ConfigureHealthChecks(this IServiceCollection services)
     {
         var databaseSettings = services.GetOptions<DatabaseSettings>(nameof(DatabaseSettings));
